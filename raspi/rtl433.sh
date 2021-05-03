2b655555495de155733053c06c5af31bc5867234
rusty <rusty.wright@gmail.com>
2021-05-02 22:56:53 -0700

    rtl433.sh

--- raspi/rtl433.sh
+++ raspi/rtl433.sh
@@ -2,4 +2,9 @@
 
 passwd=$(cat /usr/local/etc/passwd)
 
-/usr/local/bin/rtl_433 -C customary -F "mqtt://localhost:1883,user=raspi,pass=${passwd}"
+logger -i --tag rtl_433 "Starting rtl_433 to mqtt"
+# /usr/local/bin/rtl_433 -C customary -F "mqtt://localhost:1883,user=raspi,pass=${passwd},devices=rtl_433[/id]"
+/usr/local/bin/rtl_433 \
+    -C customary \
+    -F \
+    "mqtt://localhost:1883,user=raspi,pass=${passwd},devices=rtl_433[/model]"
